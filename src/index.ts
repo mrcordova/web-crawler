d64b4a7414f10f6b170c7b95ceb4be42840e9d24
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-28 10:49:46 -0800

    allow only one argument

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import { argv } from "node:process";
+function main() {
+  const cliArgs = argv.slice(2);
+  if (cliArgs.length != 1) {
+    console.log("please only provide one url");
+    process.exit(1);
+  }
+  console.log(`url given ${cliArgs[0]}`);
+  process.exit(0);
+}
+main();
